89ec960cfa1d77630d39699e8f267b7664c9dfd4
Kim-Hyunhee <tta_bi@naver.com>
2024-09-23 12:01:03 +0900

    카테고리 전체 목록 조회 API 구현

--- controller/BookController.js
+++ controller/BookController.js
@@ -2,16 +2,29 @@ const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const allBooks = (req, res) => {
-  const sql = `SELECT * FROM books;`;
+  const { categoryId } = req.query;
 
-  conn.query(sql, (err, results) => {
-    if (err) {
-      console.log(err);
-      return res.status(StatusCodes.BAD_REQUEST).end();
-    }
+  if (categoryId) {
+    const sql = `SELECT * FROM books WHERE category_id=?;`;
+    conn.query(sql, categoryId, (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+      }
 
-    res.status(StatusCodes.OK).json(results);
-  });
+      return res.status(StatusCodes.OK).json(results);
+    });
+  } else {
+    const sql = `SELECT * FROM books;`;
+    conn.query(sql, (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+      }
+
+      return res.status(StatusCodes.OK).json(results);
+    });
+  }
 };
 
 const bookDetail = (req, res) => {
@@ -32,8 +45,4 @@ const bookDetail = (req, res) => {
   });
 };
 
-const booksByCategory = (req, res) => {
-  res.json("카테고리별 도서 목록 조회");
-};
-
-module.exports = { allBooks, bookDetail, booksByCategory };
+module.exports = { allBooks, bookDetail };
