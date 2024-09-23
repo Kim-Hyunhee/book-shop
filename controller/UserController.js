6700b04b735f7036db32d4f6e0a382c62a4e0fc9
Kim-Hyunhee <tta_bi@naver.com>
2024-09-23 10:32:50 +0900

    로그인 API 구현

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,5 +1,8 @@
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const join = (req, res) => {
   const { email, password } = req.body;
@@ -17,4 +20,43 @@ const join = (req, res) => {
   });
 };
 
-module.exports = join;
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  const sql = `SELECT * FROM users WHERE email = ?`;
+  conn.query(sql, email, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    const loginUser = results[0];
+    if (loginUser && loginUser.password == password) {
+      const token = jwt.sign(
+        { email: loginUser.email },
+        process.env.PRIVATE_KEY,
+        {
+          expiresIn: "5m",
+          issuer: "hyunhee",
+        }
+      );
+
+      res.cookie("token", token, { httpOnly: true });
+      console.log(token);
+
+      return res.status(StatusCodes.OK).json(results);
+    }
+    // 401: Inauthorized (비인증; 서버가 누군지 모름) 403: Forbidden (접근 권리 없음; 서버가 누군지 알고 있음)
+    return res.status(StatusCodes.UNAUTHORIZED).end();
+  });
+};
+
+const passwordRequestReset = (req, res) => {
+  res.json("비밀번호 초기화 요청");
+};
+
+const passwordReset = (req, res) => {
+  res.json("비밀번호 초기화");
+};
+
+module.exports = { join, login, passwordRequestReset, passwordReset };
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const join = require("../controller/UserController");
+const {
+  join,
+  login,
+  passwordRequestReset,
+  passwordReset,
+} = require("../controller/UserController");
 
 router.use(express.json());
 
 router.post("/join", join);
-
-router.post("/login", (req, res) => {
-  res.json("로그인");
-});
-
-router.post("/reset", (req, res) => {
-  res.json("비밀번호 초기화 요청");
-});
-
-router.put("/reset", (req, res) => {
-  res.json("비밀번호 초기화");
-});
+router.post("/login", login);
+router.post("/reset", passwordRequestReset);
+router.put("/reset", passwordReset);
 
 module.exports = router;
