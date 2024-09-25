fe881fbd660f7db18e654aa2eac8475c45257d8f
Kim-Hyunhee <tta_bi@naver.com>
2024-09-25 11:30:55 +0900

    카테고리별, 신간, 도서 목록 조회

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,30 +1,35 @@
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
+// (카테고리별,신간 여부) 전체 도서 목록 조회
 const allBooks = (req, res) => {
-  const { categoryId } = req.query;
+  const { categoryId, news } = req.query;
 
-  if (categoryId) {
-    const sql = `SELECT * FROM books WHERE category_id=?;`;
-    conn.query(sql, categoryId, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-
-      return res.status(StatusCodes.OK).json(results);
-    });
-  } else {
-    const sql = `SELECT * FROM books;`;
-    conn.query(sql, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-
-      return res.status(StatusCodes.OK).json(results);
-    });
+  let sql = `SELECT * FROM books`;
+  let values = [];
+  if (categoryId && news) {
+    sql += ` WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
+    values = [categoryId, news];
+  } else if (categoryId) {
+    sql += ` WHERE category_id = ?`;
+    values = categoryId;
+  } else if (news) {
+    sql += ` WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
+    values = news;
   }
+
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    if (results.length) {
+      return res.status(StatusCodes.OK).json(results);
+    } else {
+      return res.status(StatusCodes.NOT_FOUND).end();
+    }
+  });
 };
 
 const bookDetail = (req, res) => {
