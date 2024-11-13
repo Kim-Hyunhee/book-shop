974dff1500b5a2a4c17bb2921d7354dc916f8731
Kim-Hyunhee <tta_bi@naver.com>
2024-11-13 10:50:16 +0900

    cors 설치

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const cors = require('cors');
 
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
 
 app.listen(process.env.PORT);
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
 
-const userRouter = require("./routes/users");
-const bookRouter = require("./routes/books");
-const categoryRouter = require("./routes/category");
-const likeRouter = require("./routes/likes");
-const cartRouter = require("./routes/carts");
-const orderRouter = require("./routes/orders");
+const userRouter = require('./routes/users');
+const bookRouter = require('./routes/books');
+const categoryRouter = require('./routes/category');
+const likeRouter = require('./routes/likes');
+const cartRouter = require('./routes/carts');
+const orderRouter = require('./routes/orders');
 
-app.use("/", userRouter);
-app.use("/books", bookRouter);
-app.use("/category", categoryRouter);
-app.use("/likes", likeRouter);
-app.use("/carts", cartRouter);
-app.use("/orders", orderRouter);
+app.use('/', userRouter);
+app.use('/books', bookRouter);
+app.use('/category', categoryRouter);
+app.use('/likes', likeRouter);
+app.use('/carts', cartRouter);
+app.use('/orders', orderRouter);
