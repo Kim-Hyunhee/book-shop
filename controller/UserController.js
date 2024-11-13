3e4f79fb8136d7e2d4371bfc022addfb0e3b2cb1
Kim-Hyunhee <tta_bi@naver.com>
2024-11-13 11:15:49 +0900

    토큰 body에 내려주기

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,8 +1,8 @@
-const conn = require("../mariadb");
-const { StatusCodes } = require("http-status-codes");
-const jwt = require("jsonwebtoken");
-const crypto = require("crypto");
-const dotenv = require("dotenv");
+const conn = require('../mariadb');
+const { StatusCodes } = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const dotenv = require('dotenv');
 dotenv.config();
 
 const join = (req, res) => {
@@ -11,10 +11,10 @@ const join = (req, res) => {
   const sql = `INSERT INTO users (email, password, salt) VALUES (?, ?, ?)`;
 
   // 회원 가입 시 비밀번호를 암호화해서 암호화 된 비밀번호와 salt값을 같이 저장
-  const salt = crypto.randomBytes(10).toString("base64");
+  const salt = crypto.randomBytes(10).toString('base64');
   const hashedPassword = crypto
-    .pbkdf2Sync(password, salt, 10000, 10, "sha512")
-    .toString("base64");
+    .pbkdf2Sync(password, salt, 10000, 10, 'sha512')
+    .toString('base64');
 
   const values = [email, hashedPassword, salt];
   conn.query(sql, values, (err, results) => {
@@ -45,8 +45,8 @@ const login = (req, res) => {
 
     // salt값 꺼내서 비밀번호를 암호화 해보고
     const hashPassword = crypto
-      .pbkdf2Sync(password, loginUser.salt, 10000, 10, "sha512")
-      .toString("base64");
+      .pbkdf2Sync(password, loginUser.salt, 10000, 10, 'sha512')
+      .toString('base64');
 
     // => 디비 비밀번호랑 비교
     if (loginUser && loginUser.password == hashPassword) {
@@ -54,15 +54,14 @@ const login = (req, res) => {
         { id: loginUser.id, email: loginUser.email },
         process.env.PRIVATE_KEY,
         {
-          expiresIn: "3m",
-          issuer: "hyunhee",
+          expiresIn: '3m',
+          issuer: 'hyunhee',
         }
       );
 
-      res.cookie("token", token, { httpOnly: true });
-      console.log(token);
+      res.cookie('token', token, { httpOnly: true });
 
-      return res.status(StatusCodes.OK).json(results);
+      return res.status(StatusCodes.OK).json({ ...results[0], token: token });
     }
     // 401: Inauthorized (비인증; 서버가 누군지 모름) 403: Forbidden (접근 권리 없음; 서버가 누군지 알고 있음)
     return res.status(StatusCodes.UNAUTHORIZED).end();
@@ -93,10 +92,10 @@ const passwordReset = (req, res) => {
 
   const sql = `UPDATE users SET password=?, salt=? WHERE email=?`;
 
-  const salt = crypto.randomBytes(10).toString("base64");
+  const salt = crypto.randomBytes(10).toString('base64');
   const hashPassword = crypto
-    .pbkdf2Sync(password, salt, 10000, 10, "sha512")
-    .toString("base64");
+    .pbkdf2Sync(password, salt, 10000, 10, 'sha512')
+    .toString('base64');
 
   const values = [hashPassword, salt, email];
   conn.query(sql, values, (err, results) => {
